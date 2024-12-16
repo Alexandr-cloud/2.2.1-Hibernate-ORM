21a6f7c623be8541fb36dfd527be0d95eefee3ae
Alexandr-cloud <kosyakov-a@mail.ru>
2024-12-16 15:31:22 +0300

    Практическая задача 2.2.1 Hibernate и ORM.

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,29 +1,46 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public Optional<User> getUserByCarModelAndSeries(String model, int series) {
+        try (Session session = sessionFactory.openSession()) {
+            String hql = "SELECT u FROM User u WHERE u.userCar.model =: model and u.userCar.series =: series";
+            Query<User> query = session.createQuery(hql, User.class);
+            query.setParameter("model", model);
+            query.setParameter("series", series);
+            return query.uniqueResultOptional();
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка получения пользователя", e);
+        }
+    }
 
 }
